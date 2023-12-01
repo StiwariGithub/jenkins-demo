67e86d2e1e878039c4df076944f5ec700ad1f222
StiwariGithub <35286152+StiwariGithub@users.noreply.github.com>
2023-12-01 19:47:29 +0530

    Update createJobs.groovy

--- jenkins-demo-master/createJobs.groovy
+++ jenkins-demo-master/createJobs.groovy
@@ -6,18 +6,3 @@ pipelineJob('pipelineJob') {
         }
     }
 }
-
-pipelineJob('theme-park-job') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url 'https://github.com/StiwariGithub/spring-boot-api-example.git'
-                    }
-                    branch 'master'
-                }
-            }
-        }
-    }
-}
